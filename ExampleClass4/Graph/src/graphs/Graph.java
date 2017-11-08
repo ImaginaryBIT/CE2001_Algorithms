81350a08f2cb6624e6e841e76f2f7e99df473646
ImaginaryBIT <32798440+ImaginaryBIT@users.noreply.github.com>
2017-11-09 00:03:25 +0800

    UPDATE

--- ExampleClass4/Graph/src/graphs/Graph.java
+++ ExampleClass4/Graph/src/graphs/Graph.java
@@ -4,7 +4,13 @@ package graphs;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
  
 class Neighbor {
     public int vertexNum;
@@ -39,7 +45,7 @@ public class Graph {
         }
          
         adjLists = new Vertex[sc.nextInt()];
- 
+        //src/exampleclass4/test.txt
         // read vertices
         for (int v=0; v < adjLists.length; v++) {
             adjLists[v] = new Vertex(sc.next(), null);
@@ -51,7 +57,7 @@ public class Graph {
             // read vertex names and translate to vertex numbers
             int v1 = indexForName(sc.next());
             int v2 = indexForName(sc.next());
-             
+
             // add v2 to front of v1's adjacency list and
             // add v1 to front of v2's adjacency list
             adjLists[v1].adjList = new Neighbor(v2, adjLists[v1].adjList);
@@ -80,16 +86,188 @@ public class Graph {
             System.out.println("\n");
         }
     }
+    public void dfs(String from_city,String to_city) {
+        // DFS uses Stack data structure
+        Stack stack = new Stack();
+        int int_from = indexForName(from_city);
+        int int_to = indexForName(to_city);
 
+
+        //list to store visited index
+        ArrayList<Integer> visited_index = new ArrayList<>();
+
+        //dictionary to keep previous routes
+        Map<String, String> previous = new HashMap<String, String>();
+
+        stack.push(adjLists[int_from]);
+        visited_index.add(int_from);
+
+        while(!stack.isEmpty()) {
+            Vertex vtx = (Vertex)stack.peek();
+            for (Neighbor nbr=vtx.adjList; nbr != null;nbr=nbr.next) {
+                //
+                if(!visited_index.contains(nbr.vertexNum)){
+                    visited_index.add(nbr.vertexNum);
+                    stack.push(adjLists[nbr.vertexNum]);
+                    
+                    //record/update the previous city
+                    previous.put(adjLists[nbr.vertexNum].name,vtx.name);
+                    
+                    //check if the city is the destination
+                    if (adjLists[nbr.vertexNum].name.equals(to_city)){
+                        
+                        //found the city backtrace the route 
+                        String parent_cty = previous.get(adjLists[nbr.vertexNum].name);
+                        System.out.println(adjLists[nbr.vertexNum].name);
+                        while(previous.get(parent_cty) != null){
+                            System.out.println("^");
+                            System.out.println(parent_cty);
+                            parent_cty = previous.get(parent_cty);
+                        }
+                        System.out.println("^");
+                        System.out.println(parent_cty);
+                        //empty the que and break the loop
+                        stack = new Stack();
+                        break;
+                    }
+                    
+                }
+
+            }
+            //stack might be empty because destination is found
+            if(!stack.isEmpty()){
+                stack.pop();
+            }
+        }
+        System.out.println("\n");
+        // Clear visited property of nodes
+        visited_index = new ArrayList<>();
+    }
+    public void bfs(String from_city,String to_city){
+        // BFS uses Queue data structure
+        Queue queue = new LinkedList();
+        int int_from = indexForName(from_city);
+        int int_to = indexForName(to_city);
+        
+        // list to store visited index
+        ArrayList<Integer> visited_index = new ArrayList<>();
+        
+        //dictionary to keep previous routes
+        Map<String, String> previous = new HashMap<String, String>();
+        
+        queue.add(adjLists[int_from]);
+        visited_index.add(int_from);
+        while(!queue.isEmpty()) {
+            Vertex vtx = (Vertex)queue.remove();
+            
+            //get all childs and check if it is already visited or not
+            for (Neighbor nbr=vtx.adjList; nbr != null;nbr=nbr.next) {
+                //
+                if(!visited_index.contains(nbr.vertexNum)){
+                    visited_index.add(nbr.vertexNum);
+                    queue.add(adjLists[nbr.vertexNum]);
+                    
+                    //record/update the previous city
+                    previous.put(adjLists[nbr.vertexNum].name,vtx.name);
+                    
+                    //check if the city is the destination
+                    if (adjLists[nbr.vertexNum].name.equals(to_city)){
+                        
+                        //found the city backtrace the route 
+                        String parent_cty = previous.get(adjLists[nbr.vertexNum].name);
+                        System.out.println(adjLists[nbr.vertexNum].name);
+                        while(previous.get(parent_cty) != null){
+                            System.out.println("^");
+                            System.out.println(parent_cty);
+                            parent_cty = previous.get(parent_cty);
+                        }
+                        System.out.println("^");
+                        System.out.println(parent_cty);
+                        //empty the que and break the loop
+                        queue = new LinkedList();
+                        break;
+                    }
+                    
+                }
+
+            }
+              
+        }
+        System.out.println("\n");
+        // Clear visited property of nodes
+        visited_index = new ArrayList<>();
+    }
     public static void main(String[] args) 
     throws IOException {
         // TODO Auto-generated method stub
+    		while(true) 
+    		{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter graph input file name: ");
         String file = sc.nextLine();
         Graph graph = new Graph(file);
-        graph.print();
- 
+        //graph.print();
+        
+        System.out.println("Enter from city : ");
+        String fromCity = sc.nextLine();
+        System.out.println("Enter to city : ");
+        String toCity = sc.nextLine();
+        while(true)
+        {
+            System.out.println("enter 1 for BFS, 2 for DFS, any other to exit");
+            int option = sc.nextInt();
+            if(option==1)
+            {
+                System.out.println("---------BFS----------");
+                
+                long start = 0;
+            
+                int runs = 10000; // enough to run for 2-10 seconds. Avoid warm up time.
+            
+                for (int i = -10000; i < runs; i++) {
+                    
+                    if (i == 0)
+                        start = System.nanoTime();
+                    
+                    // do test
+                    graph.bfs(fromCity,toCity);
+                    
+                }
+            
+                long time = System.nanoTime() - start;
+            
+                System.out.printf("The BFS took %,d ns%n", time / runs);
+            }
+            else if(option==2)
+            {
+                System.out.println("---------DFS----------");
+                
+                long start = 0;
+                
+                int runs = 10000; // enough to run for 2-10 seconds. Avoid warm up time.
+                
+                for (int i = -10000; i < runs; i++) {
+                    
+                    if (i == 0)
+                        start = System.nanoTime();
+                    
+                    // do test
+                    graph.dfs(fromCity,toCity);
+                    
+                }
+                
+                long time = System.nanoTime() - start;
+                
+                System.out.printf("The DFS took %,d ns%n", time / runs);
+            }
+            else
+            {
+                System.out.println("---------Exit----------");
+                break;
+                
+            }
+        	}
+    		}
     }
  
 }
